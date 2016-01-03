80069dd1881af1f784a1c677826047c9f619f35c
Waseem Nawaz <waseemhijodenawaz@yahoo.es>
2016-01-03 11:48:07 +0100

    added time of articles

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160102163134) do
+ActiveRecord::Schema.define(version: 20160102200549) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "image_url"
   end
 
 end
