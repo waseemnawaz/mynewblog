2c47d6fed9cb2640c17d77f2c506aca761cae6e8
Waseem Nawaz <waseemhijodenawaz@yahoo.es>
2016-01-06 00:47:08 +0100

    validations

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,8 +3,17 @@ def create
   @article = Article.find(params[:article_id])
   @comment = @article.comments.new(comment_params)
   @comment.user = current_user
-  @comment.save
-  redirect_to articles_path(@article)
+  #@comment.save
+  #redirect_to articles_path(@article)
+  respond_to do |format|
+      if @comment.save
+        format.html { redirect_to @article, notice: 'Review was created successfully.' }
+        format.json { render :show, status: :created, location: @product }
+      else
+        format.html { redirect_to @article, alert: 'Review was not saved successfully.' }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
 end
 
   def destroy
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :article
+  validates :body, presence: true
+  validates :user, presence: true
+  validates :article, presence: true
+  #validates :rating, numericality: { only_integer: true }
 end
