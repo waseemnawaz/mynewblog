5b2c3d9bfe9a7b7683d3cab632326d7b1aa4ba32
Waseem Nawaz <waseemhijodenawaz@yahoo.es>
2016-01-05 22:19:01 +0100

    added comments model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160103185956) do
+ActiveRecord::Schema.define(version: 20160105211108) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -20,6 +20,18 @@ ActiveRecord::Schema.define(version: 20160103185956) do
     t.datetime "updated_at", null: false
     t.string   "image_url"
   end
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "user_id"
+    t.text     "body"
+    t.integer  "rating"
+    t.integer  "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["article_id"], name: "index_comments_on_article_id"
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
