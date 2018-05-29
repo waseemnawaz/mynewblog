9506e3224cb944e58a69b5a2975ff0f161799b7f
waseemnawaz <waseemhijodenawaz@yahoo.es>
2018-05-29 18:54:14 +0200

    has many comments user

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ActiveRecord::Base
-  belongs_to :user, dependent: :destroy
+  belongs_to :user
   belongs_to :article
   validates :body, presence: true
   validates :user, presence: true
